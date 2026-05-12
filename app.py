66b74529b899e12946951988897e936ccc81d8a9
imaad <imaad.abr@gmail.com>
2026-05-12 21:12:49 +0200

    Added player model

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask
+from flask import Flask, redirect, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/')
+players = []
+
+@app.route("/")
 def home():
-    return "<h1>Football Club Manager </h1>"
-if __name__ == '__main__':
+    return render_template("home.html")
+
+@app.route("/add_player", methods=["GET", "POST"])
+def add_player():
+    if request.method == "POST":
+        player = {
+            "name": request.form["name"],
+            "age": request.form["age"],
+            "position": request.form["position"]
+        }
+        players.append(player)
+        return redirect("/players")
+    
+    return render_template("add_player.html")
+
+@app.route("/players")
+def view_players():
+    return render_template("players.html", players=players)
+
+if __name__ == "__main__":
     app.run(debug=True)
